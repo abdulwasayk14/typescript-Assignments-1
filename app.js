c48e8a3782a4468b605f2f65c1ab4e9ef2cf6a60
Abdul wasay khan <156195187+abdulwasayk14@users.noreply.github.com>
2024-02-27 00:46:54 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict";
+let personName = '';
+personName = prompt('what is your name?') || '';
+if (personName !== null && personName !== '')
+    (alert(`HEllo $(personName), would you like to learn some python today`));
+else
+    (alert('You have to fill your name'));
